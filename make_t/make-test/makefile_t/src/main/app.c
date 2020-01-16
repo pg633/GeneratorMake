2f1ff9e942a1030f866f99c5965c1dc6175503b0
lianzheng04 <lianzheng04@meituan.com>
2020-01-16 14:39:26 +0800

    fix

---
+++ make_t/make-test/makefile_t/src/main/app.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "math.h"
+ 
+int main(int argc, char** argv){
+    
+    printf("hello i am in main");
+    printf("==========   %d ======= ",add(1,2));
+    return 0;
+}
---
+++ make_t/make-test/src/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "math.h"
+ 
+int main(int argc, char *argv[])
+{
+    
+    printf("xxxxxxxx n" );
+    printf(" %d ",add(1,2));
+    return 0;
+}
